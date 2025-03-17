17136e5c6e0558bf72200ec8bd97f5a76f086203
Niv Benaloul <nivba2@gmail.com>
2025-03-17 22:08:48 +0200

    backend port binding

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ import { spawn } from "child_process";
 import os from "os";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Start the server
 app.listen(port, () => {
